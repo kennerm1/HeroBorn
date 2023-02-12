a08c6f56eadc82d79750b682e240388307fc9a8a
kennerm1 <123021312+kennerm1@users.noreply.github.com>
2023-02-11 17:38:52 -0700

    Code added, arena updated, levels one and two made.

--- HeroBornMCK/Assets/HeroBornMCK/Scripts/PlayerBehavior.cs
+++ HeroBornMCK/Assets/HeroBornMCK/Scripts/PlayerBehavior.cs
@@ -7,20 +7,29 @@ public class PlayerBehavior : MonoBehaviour
 
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
-
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
 
     private float vInput;
     private float hInput;
-
     private Rigidbody _rb;
+    private CapsuleCollider _col;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
     void FixedUpdate()
     {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
 
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
@@ -34,5 +43,18 @@ public class PlayerBehavior : MonoBehaviour
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+        }
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
     }
 }
